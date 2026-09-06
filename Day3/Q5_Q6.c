77e91213074098f4d0cdcebf518edc29f7341ad0
navisomehowcodes <barthwalvaishnavi02@gmail.com>
2026-09-05 23:50:48 -0700

    Day3 Q5 Q6

---
+++ Day3/Q5_Q6.c
@@ -0,0 +1,29 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include<stdio.h>
+int main()
+{
+    int cel, fah;
+    printf("Enter temperature in celsius");
+    scanf("%d", &cel);
+
+    fah=(cel*9/5)+32;
+
+    printf("Temperature in celsius=%d\n", cel);
+    printf("Temperature in fahrenheit=%d\n", fah);
+
+//Write a program to swap two numbers using a third variable.
+
+    int a, b, c;
+    printf("Enter two numbers");
+    scanf("%d %d", &a, &b);
+
+    c=b;
+    b=a;
+    a=c;
+    printf("Before swapping a=%d b=%d\n", b,a);
+    printf("After swapping a=%d b=%d\n", a,b);
+
+    return 0;
+
+}
